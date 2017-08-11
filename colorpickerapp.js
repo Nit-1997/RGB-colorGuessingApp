6a78dd608acbdf258720bbef47b0994b254207dd
Nitin Bhat <30934236+Nit-1997@users.noreply.github.com>
2017-08-12 00:58:48 +0530

    Color guessing game
    A color guessing game in which user is given with a rgb color code and 6 or 3 color options to choose from depending upon the difficulty chosen by the user .

---
+++ colorpickerapp.js
@@ -0,0 +1,139 @@
+var colors = generateRandomColors(numsq);
+ var numsq=6;
+  var squares = document.querySelectorAll(".square");
+  var rgbDisp = document.querySelector("#rgb");
+  var message = document.querySelector("#message");
+  var pickedColor=randomIndex();
+  var h1=document.querySelector("h1");
+
+   rgbDisp.textContent=pickedColor;
+for(var i = 0;i<squares.length;i++)
+   {
+      squares[i].style.backgroundColor=colors[i];
+      squares[i].addEventListener("click", function(){
+              var clickedColor=this.style.backgroundColor; 
+                if(clickedColor===pickedColor)
+                	 {
+                	 	changeColors(clickedColor);
+                	 	message.textContent="Well Played";
+                	 	h1.style.backgroundColor=pickedColor;
+                	 	reset.textContent="Play Again ";
+                	 }
+                else{
+                   this.style.backgroundColor= "#232323";
+                   message.textContent="Try Again";
+                } 
+     }); 
+
+   }   
+
+     function changeColors(color){
+       for(var i=0;i<squares.length;i++)
+       	  {
+       	  	  squares[i].style.backgroundColor=color;
+       	  }
+   }
+
+    function generateRandomColors(num){
+
+    	var arr=[];
+      for(var i=0;i<num;i++)
+      	 {
+             arr.push(generateColor());
+      	 }
+     		return arr;
+    }
+
+    function generateColor(){
+    	var r =Math.floor(Math.random()*256);
+    	var g =Math.floor(Math.random()*256);
+    	var b =Math.floor(Math.random()*256);
+
+    	return "rgb("+r+", "+g+", "+b+")";
+    } 
+ 
+    function randomIndex(){
+       var c = Math.floor(Math.random()*colors.length);
+       return colors[c];
+    }
+
+  
+ var reset = document.querySelector("#newgame");
+ 
+
+ reset.addEventListener("click", function(){
+      
+      this.textContent="New Game";
+      colors = generateRandomColors(numsq);
+      pickedColor=randomIndex();
+      rgbDisp.textContent=pickedColor;
+      h1.style.backgroundColor="steelblue";
+      message.textContent=" ";
+
+      for(var i=0;i<squares.length;i++)
+      	 {
+      	 	 squares[i].style.backgroundColor=colors[i];
+      	 }
+ });
+
+ 
+  var easybtn=document.querySelector("#easybtn");
+  var hardbtn=document.querySelector("#hardbtn");
+
+  easybtn.addEventListener("click",function(){
+      hardbtn.classList.remove("selected");
+      easybtn.classList.add("selected");
+      h1.style.backgroundColor="steelblue";
+  
+      numsq=3;
+      colors = generateRandomColors(numsq);
+      pickedColor=randomIndex();
+      rgbDisp.textContent=pickedColor;
+
+      for(var i=0;i<squares.length;i++){
+      	if(colors[i]){
+           squares[i].style.backgroundColor=colors[i];
+      	}
+      	else{
+           squares[i].style.display="none";
+      	}
+      }
+
+  });
+
+  hardbtn.addEventListener("click",function(){
+      easybtn.classList.remove("selected");
+      hardbtn.classList.add("selected");
+      h1.style.backgroundColor="steelblue";
+      numsq=6;
+      colors = generateRandomColors(numsq);
+      pickedColor=randomIndex();
+      rgbDisp.textContent=pickedColor;
+
+        for(var i=0;i<squares.length;i++){
+           squares[i].style.backgroundColor=colors[i];
+           squares[i].style.display="block";
+      }
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
